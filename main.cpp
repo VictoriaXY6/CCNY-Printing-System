c429d6b2eaca1e0d87329e507baa9723394e4ba5
Zeal Patel <zealpatel06@gmail.com>
2019-11-19 12:40:53 -0500

    Modified formatting of driver code

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ printer p1, p2, p3, p4, p5, p6, p7;
 vector<student*> studentInfo;
 vector<printer> nacPrinters = {p1, p2, p3, p4, p5, p6, p7};
 
+student s1(1111, "John Doe","johnD","john");
+
 int fastestPrinter() {
     int quickestPrinterPos = 0;
     printer quickestPrinter = nacPrinters.at(0);
@@ -24,32 +26,43 @@ int fastestPrinter() {
 }
 
 int main() {
+    studentInfo.push_back(&s1);
     int key1, key2, key3;
     string name, username, password;
     int emplID;
 
     do {
-        cout<<"Welcome to the Nac Printing System! Press 1 if Student, 2 if Admin, and 3 to quit."<<endl;
+        cout<<"Welcome to the Nac Printing System!"<<endl;
+        cout<<"Press 1 if you are a student."<<endl;
+        cout<<"Press 2 if you are an admin."<<endl;
+        cout<<"Press 3 to quit."<<endl;
+        cout<<"Enter option: ";
         cin>>key1;
+        cout << "\n------------------------------\n";
         if (key1==1) {
             do {
-                cout<<"Existing students press 1. New students press 2. Press 3 to go back."<<endl;
+                cout<<"Press 1 if you are an existing student."<<endl;
+                cout<<"Press 2 if you are a new student."<<endl;
+                cout<<"Press 3 to go back."<<endl;
+                cout<<"Enter option: ";
                 cin>>key2;
                 if (key2==1) {
-                    cout<<"Enter username";
+                    cout<<"Enter username: ";
                     cin>>username;
-                    cout<<"Enter password";
+                    cout<<"Enter password: ";
                     cin>>password;
                     for(int i=0; i<studentInfo.size(); i++) { // authentication
                         if (studentInfo.at(i)->username==username && studentInfo.at(i)->password==password) { // if user crendentials match, complete print job
                             int fastestPos=fastestPrinter();
-                            cout<<"Enter number of pages to print.";
+                            cout << "Account verified." << endl << endl;
+                            cout<<"Enter number of pages to print: ";
                             int numOfPages;
                             cin>>numOfPages;
                             studentInfo.at(i)->print(nacPrinters.at(fastestPos),numOfPages);
-                            cout<<"Hi "<<studentInfo.at(i)->name<<". Print job has been added." << endl;
+                            cout<<"Hi "<<studentInfo.at(i)->name<<". Print job has been added to printer number " << fastestPos << endl;
                             cout << "Paper Left in printer: " << nacPrinters.at(fastestPos).printerPageLimit << endl;
                             cout << studentInfo.at(i)->name <<" has " << studentInfo.at(i)->studentPageLimit << " papers left." << endl;
+                            cout << "\n------------------------------\n";
                             break;
                         }
                         else {
@@ -58,17 +71,17 @@ int main() {
                     }
                 }
                 else if (key2==2) {
-                    cout<<"Enter name";
-                    cin>>name;
-                    cout<<"Enter username";
-                    cin>>username;
-                    cout<<"Enter password";
-                    cin>>password;
-                    cout<<"Enter emplID";
+                    cout<<"Enter emplID: ";
                     cin>>emplID;
+                    cout<<"Enter name: ";
+                    cin>>name;
+                    cout<<"Enter username: ";
+                    cin>>username;
+                    cout<<"Enter password: ";
+                    cin>>password;
                     student *newStudent = new student(emplID,name,username,password);
                     studentInfo.push_back(newStudent);
-                    cout<<"New Account Created for "<<name<<"!";
+                    cout<<"New Account Created for "<<name<<"!"<<endl;
                 }
             } while (key2!=3);
         }

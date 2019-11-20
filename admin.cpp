f3281d8bf464f30b9e58e690dc3a1ffe4fbc41ff
Greg <gkimatov@gmail.com>
2019-11-20 16:34:02 -0500

    admin file

--- admin.cpp
+++ admin.cpp
@@ -4,3 +4,35 @@
 #include "student.h"
 using namespace std;
 
+void admin::clearPrinter(printer &p){
+	while(p.head != NULL){
+		p.dequeue();
+	}
+}
+
+void admin::deletePrintJob(printer &p, student &s){
+	// delete node at position associated with ID
+	node *prev = new node;
+    node *curr = p.head;
+
+    if (curr->id == s.emplID) {
+        s.studentPageLimit += curr->printOrder;
+        p.paperInPrinter += curr->printOrder;
+        p.head = p.head->next;
+    } else {
+        while (curr->id != s.emplID) {
+            prev = curr;
+            curr = curr->next;
+        }
+        node *temp = curr;
+        s.studentPageLimit += temp->printOrder;
+        p.printerPageLimit += temp->printOrder;
+        prev->next = curr->next;
+    }
+
+
+}
+
+void admin::addpaper(printer &p){
+	p.printerPageLimit = 500;
+}
